476cbea5461c96cadd49975b28c3c4e21ed41d28
Carlos Londono <c.londono.m@gmail.com>
2022-07-31 19:52:20 -0500

    refactoring code done

--- src/components/inertia/inertiaBeamIE.js
+++ src/components/inertia/inertiaBeamIE.js
@@ -1,28 +1,23 @@
 const inertiaBeamIE = (fy, fexx, hw, tw, fw, ft) => {
-  const areaFB = fw * ft;
-  const areaWeb = hw * tw;
-  const areaBeam = 2 * areaFB + areaWeb;
-  const ybf = ft / 2;
-  const yhw = hw / 2 + ft;
-  const ytf = ft / 2 + ft + hw;
-  const mbf = areaFB * ybf;
-  const mw = areaWeb * yhw;
-  const mbt = areaFB * ytf;
-  const sumM = mbf + mw + mbt;
-  const iybf = areaFB * Math.pow(ybf, 2);
-  const iyw = areaWeb * Math.pow(yhw, 2);
-  const iytf = areaFB * Math.pow(ytf, 2);
-  const sumIy = iybf + iyw + iytf;
-  const igbf = (fw * Math.pow(ft, 3)) / 12;
-  const igw = (tw * Math.pow(hw, 3)) / 12;
-  const igtf = (fw * Math.pow(ft, 3)) / 12;
-  const sumIg = igbf + igw + igtf;
+  const arrAreaBeam = [fw * ft, hw * tw, fw * ft];
+  const areaBeam = arrAreaBeam.reduce((a, b) => a + b, 0);
+  const arrY = [ft / 2, hw / 2 + ft, ft / 2 + ft + hw];
+  const arrM = arrAreaBeam.map((item, i) => item * arrY[i]);
+  const sumM = arrM.reduce((a, b) => a + b, 0);
+  const arrIy = arrAreaBeam.map((item, i) => item * Math.pow(arrY[i], 2));
+  const sumIy = arrIy.reduce((a, b) => a + b, 0);
+  const arrIg = [
+    (fw * Math.pow(ft, 3)) / 12,
+    (tw * Math.pow(hw, 3)) / 12,
+    (fw * Math.pow(ft, 3)) / 12,
+  ];
+  const sumIg = arrIg.reduce((a, b) => a + b, 0);
   const inertia = sumIy + sumIg - Math.pow(sumM, 2) / areaBeam;
   const n = sumM / areaBeam;
   const s = inertia / n;
   const fiVn = 0.9 * 0.6 * fy * (hw + 2 * ft) * tw;
-  const distNytf = ytf - n;
-  const qsv = (fiVn * areaFB * distNytf) / inertia;
+  const distNytf = arrY[2] - n;
+  const qsv = (fiVn * arrAreaBeam[0] * distNytf) / inertia;
   const e = qsv / (0.75 * 0.6 * fexx * 0.707 * 2);
   let emin = e;
   const tMax = Math.max(ft, tw);
@@ -38,6 +33,6 @@ const inertiaBeamIE = (fy, fexx, hw, tw, fw, ft) => {
   }
   const eDef = Math.max(Math.ceil(e), emin);
 
-  return [inertia, n, s, eDef];
+  return [arrAreaBeam[0], arrAreaBeam[1], areaBeam, inertia, n, s, qsv, eDef];
 };
 export default inertiaBeamIE;
